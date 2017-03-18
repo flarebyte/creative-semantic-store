970e4d81d8da5495db6426da3b45bbc8913ca453
olih <olih@users.noreply.github.com>
2017-03-18 19:19:51 +0000

    Adds some validation of configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,55 @@
+const Joi = require('joi');
+const _ = require('lodash');
 /**
  * @param {Type}
  * @return {Type}
  */
-export default function () {
-  return true
+
+const mediumString = Joi.string().min(1).max(120);
+const pathString = Joi.string().max(1000);
+const anyUrl = Joi.string().uri({scheme: ['http','https']}).max(1000);
+const authorSchema = Joi.object().keys({
+   name: mediumString,
+   url: anyUrl,
+   'twitter:username': mediumString
+});
+
+const licenseSchema = Joi.object().keys({
+   name: mediumString,
+   alternateName: mediumString,
+   description: mediumString,
+   url: anyUrl,
+   'twitter:hastag': mediumString
+});
+
+ const userConfigSchema = Joi.object().keys({
+    folders: Joi.array().items(pathString).unique().max(100),
+    activeFolder: pathString,
+    activePrefix: mediumString.required(),
+    activeAuthor: authorSchema,
+    activeLicense: licenseSchema,
+    activeLanguage: Joi.string().length(2)
+});
+const categoryMappingSchema = Joi.array().items(mediumString).length(2);
+
+const appConfigSchema = Joi.object().keys({
+   categoryMapping: Joi.array().items(categoryMappingSchema).unique().min(1).max(1000).required(),
+   onInsertEntity: Joi.func().required(),
+   onUpdateEntity: Joi.func().required()
+});
+
+const confSchema = Joi.object().keys({
+   userConfig: userConfigSchema.required(),
+   appConfig: appConfigSchema.required()
+}).required();
+
+export default function (conf) {
+  const {error, value} = Joi.validate(conf, confSchema);
+  if (error !== null) {
+    throw error;
+  }
+
+  const datastore = {};
+
+  return datastore
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,45 @@
 import test from "tape"
 import creativeSemanticStore from "../src"
 
-test("creativeSemanticStore", (t) => {
+const activeLicense =  {
+      "url":"https://creativecommons.org/licenses/by/4.0/",
+      "description":"Creative Commons Attribution 4.0 International",
+      "name":"CC BY 4.0",
+      "twitter:hastag":"#CCBY",
+      "alternateName":"CC BY"
+};
+
+const activeAuthor = {
+      "name":"Adele Smith",
+      "url":"http:/mysite.com/adele-smith",
+      "twitter:username":"@adelesmith"
+};
+const onInsertEntity = (value) => {
+  return value;
+};
+const onUpdateEntity = (value) => {
+  return value;
+};
+
+const categoryMapping = [['aphrodite','venus'],['beauty','venus'],['zeus','jupiter']]
+
+const conf = {
+  userConfig: {
+    folders: ["/proj1","/proj2/alpha"],
+    activeFolder : "active",
+    activePrefix : "1234",
+    activeAuthor,
+    activeLicense,
+    activeLanguage: "en"
+  },
+
+  appConfig: {
+    categoryMapping,
+    onInsertEntity,
+    onUpdateEntity
+  }
+};
+test("creativeSemanticStore should validate config", (t) => {
   t.plan(1)
-  t.equal(true, creativeSemanticStore(), "return true")
+  t.notEqual(creativeSemanticStore(conf), null, "return a store")
 })
